bcd977a7d645f8023dba7bdb74411f02ea4a1be8
mintaka33 <mintaka33@outlook.com>
2020-05-05 15:14:08 +0800

    rename

--- opencv_msdk/video_cap_mfx.py
+++ opencv_msdk/video_cap_mfx.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2 as cv
 import argparse
 
-cap = cv.VideoCapture('test.264', cv.CAP_INTEL_MFX)
+cap = cv.VideoCapture('/tmp/test.264', cv.CAP_INTEL_MFX)
 
 ret, frame = cap.read()
 frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
